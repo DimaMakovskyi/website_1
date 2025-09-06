7ff3b1c07b9c9013465f0fe81775b8752db27820
Your Name <you@example.com>
2025-09-06 18:43:07 +0300

    test

--- startups/filters.py
+++ startups/filters.py
@@ -7,7 +7,7 @@ class StartupFilter(df.FilterSet):
     country = df.CharFilter(field_name='location__country', lookup_expr='iexact')
     city = df.CharFilter(field_name='location__city', lookup_expr='iexact')
     min_team_size = df.NumberFilter(field_name='team_size', lookup_expr='gte')
-    funding_needed_lte = df.NumberFilter(field_name='funding_needed', lookup_expr='lte')
+    funding_needed__lte = df.NumberFilter(field_name='funding_needed', lookup_expr='lte')
     is_verified = df.BooleanFilter(field_name='is_verified')
     stage = df.CharFilter(field_name='stage', lookup_expr='iexact')
 
@@ -23,5 +23,5 @@ class StartupFilter(df.FilterSet):
         model = Startup
         fields = [
             'industry', 'industry_name', 'country', 'city',
-            'is_verified', 'min_team_size', 'funding_needed_lte', 'stage'
+            'is_verified', 'min_team_size', 'funding_needed__lte', 'stage'
         ]
