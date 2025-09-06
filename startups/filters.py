5ef211c7dd633f91a6c3d9a5be11c75b3a1fa891
Your Name <you@example.com>
2025-09-06 18:36:01 +0300

    test

--- startups/filters.py
+++ startups/filters.py
@@ -1,5 +1,4 @@
 import django_filters as df
-from django.db.models import Q
 from startups.models import Startup
 
 class StartupFilter(df.FilterSet):
@@ -8,24 +7,21 @@ class StartupFilter(df.FilterSet):
     country = df.CharFilter(field_name='location__country', lookup_expr='iexact')
     city = df.CharFilter(field_name='location__city', lookup_expr='iexact')
     min_team_size = df.NumberFilter(field_name='team_size', lookup_expr='gte')
-    funding_needed__lte = df.NumberFilter(field_name='funding_needed', lookup_expr='lte')  # <-- ім’я як у тестах
+    funding_needed_lte = df.NumberFilter(field_name='funding_needed', lookup_expr='lte')
     is_verified = df.BooleanFilter(field_name='is_verified')
     stage = df.CharFilter(field_name='stage', lookup_expr='iexact')
 
     def filter_industry(self, qs, name, value):
-        if not value:
+        if value in (None, ''):
             return qs
-        try:
-            iid = int(value)
-            return qs.filter(Q(industry_id=iid) | Q(industries__id=iid)).distinct()
-        except (TypeError, ValueError):
-            return qs.filter(
-                Q(industry__name__iexact=value) | Q(industries__name__iexact=value)
-            ).distinct()
+        v = str(value).strip()
+        if v.isdigit():
+            return qs.filter(industry_id=int(v))
+        return qs.filter(industry__name__iexact=v)
 
     class Meta:
         model = Startup
         fields = [
             'industry', 'industry_name', 'country', 'city',
-            'is_verified', 'min_team_size', 'funding_needed__lte', 'stage'
+            'is_verified', 'min_team_size', 'funding_needed_lte', 'stage'
         ]
